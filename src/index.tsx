f38f7fa27775a691a9256785c7509b0d0c05127a
nsrirang <nsrirang@iu.edu>
2023-07-22 13:43:01 -0400

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { GNUIProvider } from "@npm/grnoc-ui-provider";
 import { BrowserRouter } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -13,9 +12,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <QueryClientProvider client={client}>
       <ReactQueryDevtools />
       <BrowserRouter>
-        <GNUIProvider>
           <App />
-        </GNUIProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
